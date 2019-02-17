ad9cd9b33f761b4bf91ffb948d7a33905a656883
Zhouyu Qian <qzy@qzy.io>
2019-02-17 04:40:52 -0800

    Implement transaction inputs/outputs

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,16 @@
-import binascii
+import abc
+import base64
 import os
 import os.path
 import struct
 from dataclasses import dataclass, astuple
 from typing import *
 
-from cryptography.hazmat.primitives.asymmetric.utils import \
-    decode_dss_signature, encode_dss_signature
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric.utils import decode_dss_signature, encode_dss_signature
 from cryptography.hazmat.primitives.hashes import SHA256, Hash
 
 PRIVATE_KEY_ENCRYPTION_PASSWORD = b'passworrrrd'
@@ -19,18 +19,64 @@ HASH_DIFFICULTY = 6
 BLOCK_REWARD = 10_0000_0000
 
 
+class Serializable(abc.ABC):
+    @abc.abstractmethod
+    def to_bytes(self, b: bytearray):
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def from_bytes(b: memoryview) -> Tuple[Any, memoryview]:
+        pass
+
+
 @dataclass()
-class Transaction:
+class UTXORef(Serializable):
+    block_hash: bytes
+    index: int
+
+    FORMAT: ClassVar[struct.Struct] = struct.Struct('!32sH')
+
+    def to_bytes(self, b: bytearray):
+        b.extend(UTXORef.FORMAT.pack(*astuple(self)))
+
+    @staticmethod
+    def from_bytes(b: memoryview):
+        return UTXORef(*UTXORef.FORMAT.unpack_from(b)), b[UTXORef.FORMAT.size:]
+
+
+@dataclass()
+class UTXO(Serializable):
     amount: int
+    recipient: bytes
+
+    FORMAT: ClassVar[struct.Struct] = struct.Struct('!Q88s')
+
+    def to_bytes(self, b: bytearray):
+        b.extend(UTXO.FORMAT.pack(*astuple(self)))
+
+    @staticmethod
+    def from_bytes(b: memoryview):
+        return UTXO(*UTXO.FORMAT.unpack_from(b)), b[UTXO.FORMAT.size:]
+
+
+@dataclass()
+class Transaction(Serializable):
     payer: bytes
-    payee: bytes
+    inputs: List[UTXORef]
+    outputs: List[UTXO]
     signature: bytes = b''
 
-    FORMAT: ClassVar[struct.Struct] = struct.Struct('!Q88s88s32s32s')
+    HEADER_FORMAT: ClassVar[struct.Struct] = struct.Struct('!88sHH')
 
-    def to_signature_data(self) -> bytes:
-        return struct.pack('!QHH', self.amount, len(self.payer),
-                           len(self.payee)) + self.payer + self.payee
+    def to_signature_data(self) -> bytearray:
+        b = bytearray(
+            Transaction.HEADER_FORMAT.pack(self.payer, len(self.inputs), len(self.outputs)))
+        for txn_input in self.inputs:
+            txn_input.to_bytes(b)
+        for txn_output in self.outputs:
+            txn_output.to_bytes(b)
+        return b
 
     def verify_signature(self) -> bool:
         public_key = serialization.load_der_public_key(self.payer,
@@ -44,20 +90,28 @@ class Transaction:
         except InvalidSignature:
             return False
 
-    def to_bytes(self) -> bytes:
+    def to_bytes(self, b: bytearray):
+        b.extend(self.to_signature_data())
         r, s = decode_dss_signature(self.signature)
-        rb = r.to_bytes(32, byteorder='big')
-        sb = s.to_bytes(32, byteorder='big')
-        return Transaction.FORMAT.pack(self.amount, self.payer,
-                                       self.payee, rb, sb)
+        b.extend(r.to_bytes(32, byteorder='big'))
+        b.extend(s.to_bytes(32, byteorder='big'))
 
     @staticmethod
-    def from_bytes(b: bytes):
-        a, payer, payee, rb, sb = Transaction.FORMAT.unpack(b)
+    def from_bytes(b: memoryview):
+        payer, input_len, output_len = Transaction.HEADER_FORMAT.unpack_from(b, 0)
+        b = b[Transaction.HEADER_FORMAT.size:]
+        inputs = []
+        outputs = []
+        for i in range(input_len):
+            txn_input, b = UTXORef.from_bytes(b)
+            inputs.append(txn_input)
+        for i in range(output_len):
+            txn_output, b = UTXO.from_bytes(b)
+            outputs.append(txn_output)
+        rb, sb = b[:32], b[32:64]
         r = int.from_bytes(rb, byteorder='big')
         s = int.from_bytes(sb, byteorder='big')
-        return Transaction(amount=a, payer=payer, payee=payee,
-                           signature=encode_dss_signature(r, s))
+        return Transaction(payer=payer, inputs=inputs, outputs=outputs, signature=encode_dss_signature(r, s)), b[64:]
 
 
 @dataclass()
@@ -70,11 +124,10 @@ class Wallet:
                                             serialization.PublicFormat.SubjectPublicKeyInfo)
 
     def to_address(self) -> bytes:
-        return binascii.b2a_base64(self.serialize_public(), newline=False)
+        return base64.urlsafe_b64encode(self.serialize_public())
 
-    def create_transaction(self, amount: int, payee: bytes) -> Transaction:
-        txn = Transaction(amount=amount, payer=self.serialize_public(),
-                          payee=payee)
+    def create_transaction(self, inputs: List[UTXORef], outputs: List[UTXO]) -> Transaction:
+        txn = Transaction(payer=self.serialize_public(), inputs=inputs, outputs=outputs)
         txn.signature = self.private_key.sign(txn.to_signature_data(),
                                               ec.ECDSA(SHA256()))
         return txn
@@ -83,8 +136,7 @@ class Wallet:
         s = self.private_key.private_bytes(encoding=serialization.Encoding.PEM,
                                            format=serialization.PrivateFormat.PKCS8,
                                            encryption_algorithm=serialization.BestAvailableEncryption(
-                                               PRIVATE_KEY_ENCRYPTION_PASSWORD)
-                                           )
+                                               PRIVATE_KEY_ENCRYPTION_PASSWORD))
         with open(os.path.expanduser(PRIVATE_KEY_PATH), 'wb') as f:
             f.write(s)
 
@@ -114,21 +166,21 @@ class Wallet:
 
 
 @dataclass()
-class Block:
+class Block(Serializable):
     transactions: List[Transaction]
     nonce: int = 0
     parent_hash: bytes = b'\x00' * 32
     block_hash: bytes = b'\x00' * 32
 
+    HEADER_FORMAT: ClassVar[struct.Struct] = struct.Struct('!Q32sL')
+
     def to_hash_challenge(self) -> bytearray:
-        b = bytearray(struct.pack('!Q32sL', self.nonce, self.parent_hash,
-                                  len(self.transactions)))
+        b = bytearray(Block.HEADER_FORMAT.pack(self.nonce, self.parent_hash, len(self.transactions)))
         for t in self.transactions:
-            b.extend(t.to_bytes())
+            t.to_bytes(b)
         return b
 
-    def solve_hash_challenge(self, difficulty: int = HASH_DIFFICULTY,
-                             max_tries=None):
+    def solve_hash_challenge(self, difficulty: int = HASH_DIFFICULTY, max_tries=None):
         b = self.to_hash_challenge()
         if max_tries is None:
             max_tries = 1 << 64
@@ -143,23 +195,19 @@ class Block:
             else:
                 self.nonce += 1
                 self.nonce %= 1 << 64
-                b[0:8] = struct.pack('!Q', self.nonce)
+                struct.pack_into('!Q', b, 0, self.nonce)
 
-    def to_bytes(self) -> bytes:
-        b = self.to_hash_challenge()
+    def to_bytes(self, b: bytearray):
+        b.extend(self.to_hash_challenge())
         b.extend(self.block_hash)
-        return bytes(b)
 
     @staticmethod
-    def from_bytes(b: bytes):
-        f = '!Q32sL'
-        sz = struct.calcsize(f)
-        nonce, parent_hash, txn_len = struct.unpack(f, b[:sz])
+    def from_bytes(b: memoryview):
+        nonce, parent_hash, transactions_len = Block.HEADER_FORMAT.unpack_from(b)
+        b = b[Block.HEADER_FORMAT.size:]
         transactions = []
-        for i in range(txn_len):
-            txn = Transaction.from_bytes(b[sz:sz + Transaction.FORMAT.size])
-            sz += Transaction.FORMAT.size
+        for i in range(transactions_len):
+            txn, b = Transaction.from_bytes(b)
             transactions.append(txn)
-        block_hash = b[sz:sz + 32]
-        return Block(transactions=transactions, nonce=nonce,
-                     parent_hash=parent_hash, block_hash=block_hash)
+        block_hash = b[:32]
+        return Block(transactions=transactions, nonce=nonce, parent_hash=parent_hash, block_hash=block_hash), b[32:]
